7629d54074405b5fbcf6f7dbb0e5725b9ad201f3
Andrés66666 <cya2024773@est.univalle.edu>
2025-10-06 23:21:27 -0400

    Primer commit DB

--- main/settings.py
+++ main/settings.py
@@ -194,3 +194,16 @@ if not DEBUG:
 # DEFAULTS
 # ========================================
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "DEBUG",
+    },
+}
