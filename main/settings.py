6449f0e2b4ae8f3b46371b03d51407cb00b4d89d
Andrés66666 <cya2024773@est.univalle.edu>
2025-10-08 23:04:22 -0400

    Primer commit DB

--- main/settings.py
+++ main/settings.py
@@ -208,6 +208,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
     "https://mallafinita.netlify.app",
+    "https://backendecuacion-1.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS.copy()
