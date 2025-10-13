447fbaa5bf2151999aa08b912258bdc4c52d7d4c
freemarket0620 <freemarket0620@gmail.com>
2025-10-12 21:33:46 -0400

    Primer commit DB

--- main/settings.py
+++ main/settings.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "GuardianUnivalle_Benito_Yucra.detectores.detector_dos.DOSDefenseMiddleware",
+    "users.middleware.AuditoriaMiddleware",
 ]
 
 # =====================================================
